fff39bc0e0ef107cb4fc3ca3cca6838e8f10bbf7
BLINDEY3 <atvandsh@gmail.com>
2024-03-11 18:11:02 +0300

    added gui & updated to python 3.12 ver0.2.1

---
+++ gui.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode("Light")
+ctk.set_default_color_theme("blue")
+
+window = ctk.CTk()
+window.title("LogReader by s1lencer")
+window.geometry("900x600")
+
+window.mainloop()
